041f13d3d8cd684f194a22b1e5cc730b437be48d
Ayush <ayush.up105@gmail.com>
2023-10-06 21:16:52 +0530

    Responsiveness

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,11 +1,13 @@
 import { styled } from "styled-components";
 import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${mobile({ padding: "10px", flexDirection:"column" })}
 `;
 const ImageContainer = styled.div`
   flex: 1;
@@ -14,10 +16,12 @@ const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
+  ${mobile({ height: "40vh" })}
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 20px;
+  ${mobile({ padding: "10px" })}
 `;
 const Title = styled.h1`
   font-weight: 200;
@@ -35,6 +39,7 @@ const FilterContainer = styled.div`
   justify-content: space-between;
   width: 50%;
   margin: 30px 0px;
+  ${mobile({ width: "100%" })}
 `;
 const Filter = styled.div`
   display: flex;
@@ -63,6 +68,7 @@ const AddContainer = styled.div`
     align-items: center;
     width: 50%;
     justify-content: space-between;
+    ${mobile({ width: "100%" })}
 `;
 const AmountContainer = styled.div`
     display: flex;
