5a8d0f9890023da8eabde958505280c6a9cb3d1e
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-10-06 17:31:27 +0530

    Setup Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,17 @@ import PrivateComponent from './components/PrivateComponent'
 const App = () => {
   return (
     <>
-        <Router>
-          <NavBar/>
-          <Routes>
-            <Route element={<PrivateComponent />}>
-              <Route path="/logout" element={<h1>logout</h1>} />
-            </Route>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route element={<PrivateComponent />}>
+            <Route path="/logout" element={<h1>logout</h1>} />
+          </Route>
 
-            <Route path="/register" element={<Register/>} />
-            <Route path="/login" element={<Login/>} />
-          </Routes>
-        </Router>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </>
   )
 }
