351f58b06df32c46dbcb089878ab3349f91452d5
Soham <realsoham774@gmail.com>
2023-10-07 08:23:39 +0530

    added styling to login and register

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,74 +1,92 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Loader from "../../components/loader/Loader";
+  // Login.js
 
-const Login = () => {
-  const [isloading, setIsLoading]=useState(false);
-  const navigate = useNavigate();
-   //very imp to note //using this we check if data is stored in local storage and navigate the user to "/"
-  useEffect(() => {
-    const auth = JSON.parse(localStorage.getItem("user"));
-    if (auth) {
-      navigate("/");
-    }
-  }, []);
+  import React, { useState, useEffect } from "react";
+  import { useNavigate } from "react-router-dom";
+  import Loader from "../../components/loader/Loader";
+  import "./login.css"; // Import the CSS file
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async () => {
-    try {
-      setIsLoading(true);
-      const result = await fetch("https://shopnest-backend.onrender.com/api/auth/login", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
-
-      const data = await result.json();
-
-      if (result.status === 500) {
-        let errorMessage=data.error;
-        alert(errorMessage);
-      } if (result.status === 400) {
-        let errorMessages = data.errors.map(error => error.msg).join('\n');
-        alert(errorMessages);
-      } else if(result.status === 404) {
-        let errorMessage = data.error;
-        alert(errorMessage); 
-      } else if (result.status === 200) {
-        localStorage.setItem("user", JSON.stringify(data)); 
-        navigate('/');
+  const Login = () => {
+    const [isloading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
+     //very imp to note //using this we check if data is stored in local storage and navigate the user to "/"
+    useEffect(() => {
+      const auth = JSON.parse(localStorage.getItem("user"));
+      if (auth) {
+        navigate("/");
       }
-    } catch (error) {
-      console.error(error);
-      alert("An error occurred while logging in.");
-    } finally {
-      setIsLoading(false); 
-    }
+    }, []);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+      try {
+        setIsLoading(true);
+        const result = await fetch("https://shopnest-backend.onrender.com/api/auth/login", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email, password })
+        });
+
+        const data = await result.json();
+
+        if (result.status === 500) {
+          let errorMessage = data.error;
+          alert(errorMessage);
+        } if (result.status === 400) {
+          let errorMessages = data.errors.map(error => error.msg).join('\n');
+          alert(errorMessages);
+        } else if (result.status === 404) {
+          let errorMessage = data.error;
+          alert(errorMessage);
+        } else if (result.status === 200) {
+          localStorage.setItem("user", JSON.stringify(data));
+          navigate('/');
+        }
+      } catch (error) {
+        console.error(error);
+        alert("An error occurred while logging in.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    return (
+      <>
+        {isloading == true ? (
+          <Loader />
+        ) : (
+          <div className="loginContainer">
+            <div className="login">
+              <h1 className="head">LOGIN</h1>
+              <input
+                className="inputBox" // Use CSS classes defined in Login.css
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email"
+              />
+              <input
+                className="inputBox" // Use CSS classes defined in Login.css
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+              />
+              <button
+                className="loginbutton" // Use CSS classes defined in Login.css
+                type="button"
+                onClick={handleLogin}
+              >
+                LOGIN UP
+              </button>
+            </div>
+          </div>
+        )}
+      </>
+    );
   };
 
-  return (
-    <>
-    {
-      isloading==true ?
-      (
-        <Loader/>
-      ):(
-      <div className="loginContainer">
-      <div className="login">
-        <h1>LOGIN</h1>
-        <input className='inputBox' type='text' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Enter Email' />
-        <input className='inputBox' type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter Password' />
-        <button className='loginbutton' type='button' onClick={handleLogin}>LOGIN UP</button>
-      </div>
-      </div>
-      )
-    }
-    </>
-  );
-};
-
-export default Login;
+  export default Login;
